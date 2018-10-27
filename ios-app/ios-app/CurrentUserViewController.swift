97d7558abe317405034fa15a46a2c8308986eb45
TaigaMikami <mikamimikami60@gmail.com>
2018-10-27 15:49:39 +0900

    サーバーサイドのテーブルに合わせてクライアントモデルの定義

---
+++ ios-app/ios-app/CurrentUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentUserViewController.swift
+//  ios-app
+//
+//  Created by 三上大河 on 2018/10/27.
+//  Copyright © 2018年 三上大河. All rights reserved.
+//
+
+import UIKit
+
+class CurrentUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
