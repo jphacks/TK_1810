8cb666baade80923725ef3b8385b913224229b5a
TaigaMikami <mikamimikami60@gmail.com>
2018-10-27 14:50:44 +0900

    ios-app Initialize commit

---
+++ ios-app/ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-app
+//
+//  Created by 三上大河 on 2018/10/27.
+//  Copyright © 2018年 三上大河. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
