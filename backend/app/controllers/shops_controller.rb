84a5875a3374dfcb7fcb8fe6c7467b080955e8ff
Kyou13 <kyou1013@gmail.com>
2018-10-28 09:53:43 +0900

    add shop search func with geocode

--- backend/app/controllers/shops_controller.rb
+++ backend/app/controllers/shops_controller.rb
@@ -5,8 +5,27 @@ class ShopsController < ApplicationController
 
   def show
     @shop = Shop.find_by(id: params[:id])
-    
+
     if @shop.nil?
+      @status = "error"
+      @error = "お店が見つかりませんでした"
+      return
+    end
+  end
+
+  def search
+    if params[:lat].blank? || params[:long].blank?
+      @status = "error"
+      @error = "必須パラメータがありません"
+      return
+    end
+
+    latitude = params[:lat]
+    longitude = params[:long]
+    distance = 5
+    @shops = Shop.near([latitude, longitude], distance, units: :km)
+
+    if @shops.empty?
       @status = "error"
       @error = "お店が見つかりませんでした"
       return
